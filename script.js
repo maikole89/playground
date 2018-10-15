0cd97a94a8a0c52546e1e271d33d190e951e7b2d
Maikol <maikole89@gmail.com>
2018-10-15 13:56:36 -0400

    alert, console.log, prompt

---
+++ script.js
@@ -0,0 +1,11 @@
+// var userName = prompt("what is your name?");
+// alert("Nice to meet you, " + userName);
+// console.log("Also great to meet you, " + userName);
+
+
+var firstName = prompt("What is your first name?");
+var lastName = prompt("What is your last name?");
+var userAge = prompt("What is your age?");
+
+console.log("Your full name is " + firstName + " " + lastName);
+console.log("Your are " + userAge + " years old.");
