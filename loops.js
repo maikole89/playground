45e3fd74d46547db7a60e54acf02d87603547054
Maikol <maikole89@gmail.com>
2018-10-18 12:06:34 -0400

    forLoop exercise

---
+++ loops.js
@@ -0,0 +1,33 @@
+console.log("print all numbers between -10 and 19")
+
+for(var i = -10; i < 20; i++) {
+  console.log(i)
+}
+
+console.log("print all even between 10 and 40")
+
+// for(var i = 10; i <= 40; i+=2) {
+//   console.log(i)
+// }
+
+for(var i = 10; i <= 40; i+=1) {
+  if(i % 2 === 0) {
+    console.log(i)
+  }
+}
+
+console.log("printing all odd numbers between 300 and 333")
+for(var i = 300; i <= 333; i++) {
+  if(i % 2 !== 0) {
+    console.log(i);
+  }
+}
+
+
+console.log("print all numbers divisible by 5 and 50")
+
+for(var i = 5; i <= 50; i++) {
+  if(i % 5=== 0 && i % 3 === 0) {
+    console.log(i);
+  }
+}
