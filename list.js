6e9e457b07f6fd8e2bf14447bbb27232b7a63785
Maikol <maikole89@gmail.com>
2018-11-01 12:05:16 -0400

    Basic todo list

--- list.js
+++ list.js
@@ -2,6 +2,17 @@ var todos = ["Buy new turtle"];
 
 var input = prompt("What would you like to do?")
 
-if(input === "list") {
-  console.log(todos);
+while(input !== "quit") {
+  //handle input
+  if(input === "list") {
+    console.log(todos);
+  } else if (input === "new") {
+    //askf or new todo
+    var newTodo = prompt("Enter new todo");
+    // add to todos array
+    todos.push(newTodo);
+  }
+  //ask again for new input
+  input = prompt("What would you like to do?")
 }
+console.log("Ok, you quit the app!")
